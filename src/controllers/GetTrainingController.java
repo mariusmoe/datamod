69c2a5e40c29b0393a0f63acb6741ab13c28f500
Marius Oscar Moe <mariuom@stud.ntnu.no>
2016-03-10 09:40:59 +0100

    Add folder structure and initial mainframe with tabs
    Some easy table output sketches. further pushes will be done in the
    branch "moedev".

---
+++ src/controllers/GetTrainingController.java
@@ -0,0 +1,33 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class GetTrainingController {
+
+    @FXML
+    private TableColumn<?, ?> performanceColumn;
+
+    @FXML
+    private ChoiceBox<?> trainingBox;
+
+    @FXML
+    private TableView<?> trainingTable;
+
+    @FXML
+    private TableColumn<?, ?> goalColumn;
+
+    @FXML
+    private TableColumn<?, ?> dateColumn;
+
+    @FXML
+    private TableColumn<?, ?> durationColumn;
+
+    @FXML
+    private TableColumn<?, ?> howColumn;
+
+
+
+}
---
+++ src/dagbokapplication/UserApplication.java
@@ -0,0 +1,41 @@
+package dagbokapplication;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Main application for the
+ * cabin group
+ *
+ */
+public class UserApplication extends Application{
+	
+	/**
+	 * Start application
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("../fxml/UserMainFrame.fxml"));
+        //primaryStage.getIcons().add(new Image("/koieapplication/cabin.png"));
+        primaryStage.setTitle("Show training");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setMinHeight(625);
+        primaryStage.setMinWidth(650);
+        primaryStage.show();
+        
+        
+        
+        
+    }
+
+	/**
+	 * Launch
+	 * @param args None needed
+	 */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
