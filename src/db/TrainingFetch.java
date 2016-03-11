90fad89721a3674597b2fc5acf93ca186267d025
Marius Oscar Moe <mariuom@stud.ntnu.no>
2016-03-11 13:48:01 +0100

    add connection to local database

--- src/db/TrainingFetch.java
+++ src/db/TrainingFetch.java
@@ -25,35 +25,33 @@ public class TrainingFetch {
 
 	private ResultSet resultSet = null;
 	
-	
-	
 	/**
 	 * Get database results for given training
 	 * @param name training to look up
 	 * @throws Exception Exceptions if connection fails
 	 */
 	public void readDataBase(String name) throws Exception {
-		
+		System.out.println("trid to connect...");
 		try {
 			//mysql.stud.ntnu.no
 			// connect = DriverManager.getConnection("jdbc:mysql://localhost/dag?" + "user=mariuom_koie&password=eple");
-			connect = DriverManager.getConnection("jdbc:mysql://localhost/dag?", "user", "pass");
+			// connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dag" , "root", null);
+			connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/dag","root","eple");
 	     // Connection connection = DriverManager.getConnection("jdbc:mysql://129.241.160.113/mariuom_koiedb?" + "user=mariuom_koie&password=eple");
 
 			statement = connect.createStatement();
 	      // Result set get the result of the SQL query
-			resultSet = statement.executeQuery("select * from dag.trening where idtrening = '" + name + "'");
+			resultSet = statement.executeQuery("select * from dag.trening"); //where idtrening = '" + name + "'");
+			System.out.println("Connection SUCCESS - Querry SUCCESS");
 			writeResultSet(resultSet);
 	      
 	    } catch (Exception e) {
+	    	System.out.printf("failed" , e.toString());
 	    	throw e;
 	    } finally {
 	    	close();
 	    }
-		
-		
 	}
-	
 	
 	/**
 	 * Process results from database
@@ -69,9 +67,7 @@ public class TrainingFetch {
 			personalFitList.add(resultSet.getInt("personlig_form"));
 			acchivementList.add(resultSet.getInt("prestasjon"));
 	    }
-
 	  }
-	
 	
 	/**
 	 * Close database connection
@@ -107,7 +103,6 @@ public class TrainingFetch {
 	public ArrayList<Integer> getDurationList() {
 		return durationList;
 	}
-
 
 	/**
 	 * @return the personalFitList
