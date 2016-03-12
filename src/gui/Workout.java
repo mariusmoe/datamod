be98d4d2bb727e18235821286ce4ced3d7152361
Hallgeir <hallgegu@stud.ntnu.no>
2016-03-12 14:32:13 +0100

    Utvidet Workout med feltene til innendørs- og utendørstrening, legger til GUI-elementer for disse i registerPane

--- src/gui/Workout.java
+++ src/gui/Workout.java
@@ -6,16 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Workout {
-	
-	
+
 	private LocalDate date;
 	private LocalTime time;
 	private int duration;
 	private int form;
 	private int achievement;
 	private String note;
-	
+
+	//Outdoor fields
+	private int temp;
+	private String weather;
+
+	//Indoor fields
+	private int spectatorsNum;
+	private int airquality;
+
 	private List<String> exerciseIds = new ArrayList<>();
+
 	
 
 }
