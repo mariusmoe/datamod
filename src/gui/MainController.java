63e6cedf495c48e79f52c30651557230f1f97ce6
Hallgeir <hallgegu@stud.ntnu.no>
2016-03-13 16:49:27 +0100

    Felter for utendørs- og innendørstrening blir disabled for henholdsvis innendørs-toggle og utendørs-toggle

--- src/gui/MainController.java
+++ src/gui/MainController.java
@@ -1,16 +1,25 @@
 package gui;
-
-
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
-
+import javafx.scene.paint.Paint;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainController {
-	
+
+	@FXML private Label airqText;
+	@FXML private Slider airqSlider;
+	@FXML private TextField specsNum;
+
+	@FXML private TextField tempField;
+	@FXML private TextArea weatherArea;
+
+	@FXML private ToggleButton indoorToggle;
+	@FXML private ToggleButton outdoorToggle;
+
 	@FXML private VBox exContainer;
 	@FXML private ComboBox<String> hour;
 	@FXML private ComboBox<String> minute;
@@ -18,20 +27,84 @@ public class MainController {
 	@FXML private ExercisePaneController expController;
 	@FXML private TextField weight;
 
-    @FXML
-    private TextField noSets;
+    @FXML private TextField noSets;
 
-    @FXML
-    private TextField noReps;
+    @FXML private TextField noReps;
 
 	
 	@FXML
     void saveExercise() {
 
     }
-	
+
+	void indoorToggled(){
+		indoorEnabler(true);
+		outdoorEnabler(false);
+
+	}
+
+	private void indoorEnabler(boolean enable) {
+		if (enable){
+			airqText.setTextFill(Paint.valueOf("black"));
+			airqSlider.setDisable(false);
+			specsNum.setDisable(false);
+			}
+		else{
+			airqText.setTextFill(Paint.valueOf("grey"));
+			airqSlider.setDisable(true);
+			specsNum.setDisable(true);
+		}
+	}
+
+	void outdoorToggled(){
+		indoorEnabler(false);
+		outdoorEnabler(true);
+
+	}
+
+	private void outdoorEnabler(boolean enabler) {
+		if (enabler){
+			tempField.setDisable(false);
+			weatherArea.setDisable(false);
+
+		}
+		else {
+			tempField.setDisable(true);
+			weatherArea.setDisable(true);
+		}
+	}
+
+
 	public void initialize(){
+		indoorToggle.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> property, Boolean oldValue, Boolean newValue){
+				if (newValue) {
+					indoorToggled();
+					outdoorToggle.setSelected(false);
+				}
+
+				else {
+					outdoorToggle.setSelected(true);
+				}
+
+
+			}
+
+		});
+		outdoorToggle.selectedProperty().addListener(new ChangeListener<Boolean>() {
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if (newValue) {
+					outdoorToggled();
+					indoorToggle.setSelected(false);
+				}
+				else
+					indoorToggle.setSelected(true);
+			}
+		});
 		fillComboBox();
+
 		/*ReadOnlyObjectProperty<Exercise> selectionProperty = exercises.getSelectionModel().selectedItemProperty();
 		selectionProperty.addListener((property, oldValue, newValue) -> {
 			expController.exercisePicked(newValue);
