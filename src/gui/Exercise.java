47ba61ccfca433308485246a86d30cfa53e22a23
Hallgeir <hallgegu@stud.ntnu.no>
2016-03-15 11:45:51 +0100

    La til felt, getter og setter for kategori og alternativ øvelse i Exercise-klassen

--- src/gui/Exercise.java
+++ src/gui/Exercise.java
@@ -4,8 +4,20 @@ import javafx.beans.property.Property;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Exercise {
+
+	public Exercise(String name, String description, String alternative, String category){
+		setName(name);
+		setDescription(description);
+		setAlternative(alternative);
+		setCategory(category);
+	}
+
+
 	private Property<String> nameProperty = new SimpleStringProperty();
 	private Property<String> descriptionProperty = new SimpleStringProperty();
+	private Property<String> alternativeProperty = new SimpleStringProperty();
+	private Property<String> categoryProperty = new SimpleStringProperty();
+
 
 	void setDescription(String descr){
 		descriptionProperty.setValue(descr);
@@ -19,6 +31,19 @@ public class Exercise {
 	String getName(){
 		return nameProperty.getValue();
 	}
+	void setAlternative(String alternative){
+		alternativeProperty.setValue(alternative);
+	}
+	String getAlternative(){
+		return alternativeProperty.getValue();
+	}
+	void setCategory(String category) {
+		categoryProperty.setValue(category);
+	}
+	String getCategory(){
+		return categoryProperty.getValue();
+	}
+
 
 
 }
