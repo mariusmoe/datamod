44461c3c1772cb6c2dbc3f212b56c06c9261e977
Hallgeir <hallgegu@stud.ntnu.no>
2016-03-17 18:57:37 +0100

    La til noen getters

--- src/models/Workout.java
+++ src/models/Workout.java
@@ -1,6 +1,8 @@
 package models;
 
+import java.sql.Timestamp;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,39 @@ public class Workout {
 
 	}
 
+	public Timestamp getDateAsTimestamp(){
+		LocalDateTime datetime = getDate().atTime(getTime());
+		Timestamp timestamp = Timestamp.valueOf(datetime);
+		return timestamp;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public int getAchievement() {
+		return achievement;
+	}
+
+	public int getForm() {
+		return form;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public List<String> getExerciseIds() {
+		return exerciseIds;
+	}
+
 	private LocalDate date;
 	private LocalTime time;
 	private int duration;
@@ -18,12 +53,29 @@ public class Workout {
 	private int achievement;
 	private String note;
 
+	public int getTemp() {
+		return temp;
+	}
+
+	public String getWeather() {
+		return weather;
+	}
+
 	//Outdoor fields
 	private int temp;
 	private String weather;
 
 	//Indoor fields
 	private int spectatorsNum;
+
+	public int getAirquality() {
+		return airquality;
+	}
+
+	public int getSpectatorsNum() {
+		return spectatorsNum;
+	}
+
 	private int airquality;
 
 	private List<String> exerciseIds = new ArrayList<>();
@@ -36,6 +88,7 @@ public class Workout {
 		this.temp = temp;
 		this.weather = weather;
 	}
+
 
 	
 
