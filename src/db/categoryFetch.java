a5b70ca09e15b0cc7d4235066b7e76442b25e3ea
Hallgeir <hallgegu@stud.ntnu.no>
2016-03-30 21:13:54 +0200

    Added William's categoryFetch

---
+++ src/db/categoryFetch.java
@@ -0,0 +1,51 @@
+package db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Created by William on 14.03.2016.
+ */
+public class CategoryFetch{
+
+    private ArrayList<String> categories = new ArrayList<String>();
+
+    private static Connection connect = null;
+    private static Statement statement = null;
+
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/dag";
+    static final String USER = "root";
+    static final String PASS = "eple";
+
+    public ArrayList<String> getCategories(){
+        try {
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to a selected database...");
+            connect = DriverManager.getConnection(DB_URL, USER, "eple");
+            System.out.println("Connected database successfully...");
+            PreparedStatement prstmnt = null;
+            String get = "SELECT knavn from dag.kategori";
+            prstmnt = connect.prepareStatement(get);
+            ResultSet set = prstmnt.executeQuery();
+            while (set.next()){
+                categories.add(set.getString("knavn"));
+            }
+            set.close();
+        } catch (SQLException se) {
+            se.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null)
+                    connect.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        }
+        
+        return categories;
+    }
+}
